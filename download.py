5a53e2c2816bc5d44dd35207553b99f5c387e43f
tohli <yusuf.coskun1@boun.edu.tr>
2023-01-31 10:58:29 +0300

    YT to MP3/MP4 Converter

---
+++ download.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+
+def Download(link):
+  youtubeObject = YouTube(link)
+  youtubeObject = youtubeObject.streams.get_highest_resolution()
+  try:
+      youtubeObject.download()
+  except:
+    print("Error")
+  print("This download has completed.")
+
+link = input("Enter video link:")
+Download(link)
+
